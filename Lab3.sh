89809549428028a81cdcbf7928b21556e2eeff01
SimranDeep987 <165347431+SimranDeep987@users.noreply.github.com>
2024-04-05 17:59:39 -0400

    Lab3.sh

--- Lab3.sh
+++ Lab3.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
-# function to execute a command and check if it succeeded
-execute_command() {
-    if ! "$@"; then
-        echo "Error executing: $@"
+
+usage() {
+    echo "Usage: $0 [-v]"
+    echo "Options:"
+    echo "  -v, --verbose    Run in verbose mode"
+    exit 1
+}
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -v|--verbose) VERBOSE=true; shift ;;
+        *) echo "Unknown option: $1"; usage ;;
+    esac
+done
+
+run_command() {
+    local cmd="$1"
+    local desc="$2"
+    if $cmd; then
+        echo "$desc completed successfully."
+    else
+        echo "Error: $desc failed."
         exit 1
     fi
 }
 
-verbose_flag=""
+run_command "scp configure-host.sh remoteadmin@server1-mgmt:/root" "Transfer configure-host.sh to server1-mgmt"
+run_command "ssh remoteadmin@server1-mgmt -- /root/configure-host.sh -name loghost -ip 192.168.16.3 -hostentry webhost 192.168.16.4 $([[ "$VERBOSE" == true ]] && echo '-verbose')" "Run configure-host.sh on server1-mgmt"
+run_command "scp configure-host.sh remoteadmin@server2-mgmt:/root" "Transfer configure-host.sh to server2-mgmt"
+run_command "ssh remoteadmin@server2-mgmt -- /root/configure-host.sh -name webhost -ip 192.168.16.4 -hostentry loghost 192.168.16.3 $([[ "$VERBOSE" == true ]] && echo '-verbose')" "Run configure-host.sh on server2-mgmt"
+run_command "./configure-host.sh -hostentry loghost 192.168.16.3 $([[ "$VERBOSE" == true ]] && echo '-verbose')" "Run configure-host.sh locally for loghost entry"
+run_command "./configure-host.sh -hostentry webhost 192.168.16.4 $([[ "$VERBOSE" == true ]] && echo '-verbose')" "Run configure-host.sh locally for webhost entry"
 
-# Check if the script is in verbose mode
-if [ "$1" = "-v" ]; then
-    verbose_flag="-v"
-fi
-
-
-scp configure-host.sh remoteadmin@server1-mgmt:/root
-ssh remoteadmin@server1-mgmt -- /root/configure-host.sh -verbose -name loghost -ip 192.168.16.3 -hostentry webhost 192.168.16.4
-
-# Transfer configure-host.sh to server2-mgmt and execute
-scp configure-host.sh remoteadmin@server2-mgmt:/root
-ssh remoteadmin@server2-mgmt -- /root/configure-host.sh -verbose -name webhost -ip 192.168.16.4 -hostentry loghost 192.168.16.3
-
-# Update local /etc/hosts
-./configure-host.sh -verbose -hostentry loghost 192.168.16.3
-./configure-host.sh -verbose -hostentry webhost 192.168.16.4
+echo "All configurations applied successfully."
