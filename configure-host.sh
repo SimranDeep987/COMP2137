41f948578a94fe6ab4745bb8d8ff9e29a2c0e935
SimranDeep987 <165347431+SimranDeep987@users.noreply.github.com>
2024-04-05 17:54:18 -0400

    Update configure-host.sh

--- configure-host.sh
+++ configure-host.sh
@@ -1,49 +1,57 @@
-log_changes() {
-    [ "$VERBOSE" = true ] && echo "$1"
-    logger "$1"
-}
+#!/bin/bash
 
-update_hostname() {
-    local desired_name="$1"
-    if [ "$desired_name" != "$(hostname)" ]; then
-        hostnamectl set-hostname "$desired_name"
-        sed -i "s/$(hostname)/$desired_name/g" /etc/hosts
-        echo "$desired_name" > /etc/hostname
-        log_changes "Hostname updated to $desired_name"
+# Function to log messages if VERBOSE is true
+log_message() {
+    if [ "$VERBOSE" = true ]; then
+        echo "$1"
     fi
 }
 
-update_ip() {
-    local desired_ip="$1"
-    if [ "$desired_ip" != "$(hostname -I | awk '{print $1}')" ]; then
-        sed -i "/^.*$(hostname -I | awk '{print $1}').*/c\\$desired_ip $HOSTNAME" /etc/hosts
-        sed -i "s/address .*/address $desired_ip/g" /etc/netplan/*.yaml
-        netplan apply
-        log_changes "IP address updated to $desired_ip"
+# Configure hostname
+if [ -n "$NEW_HOSTNAME" ]; then
+    CURRENT_HOSTNAME=$(hostname)
+    if [ "$CURRENT_HOSTNAME" != "$NEW_HOSTNAME" ]; then
+        echo "$NEW_HOSTNAME" > /etc/hostname
+        sed -i "s/^.*$CURRENT_HOSTNAME/$(echo $NEW_HOSTNAME | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/" /etc/hosts
+        log_message "Hostname updated to $NEW_HOSTNAME"
+        logger "Hostname updated to $NEW_HOSTNAME"
     fi
-}
+fi
+
+# Configure IP address
+if [ -n "$NEW_IP" ]; then
+    CURRENT_IP=$(hostname -I | awk '{print $1}')
+    if [ "$CURRENT_IP" != "$NEW_IP" ]; then
+        sed -i "s/$CURRENT_IP/$NEW_IP/g" /etc/hosts
+        sed -i "s/address .*/address $NEW_IP/g" /etc/netplan/*.yaml
+        log_message "IP address updated to $NEW_IP"
+        logger "IP address updated to $NEW_IP"
+    fi
+fi
 
 update_host_entry() {
-    local desired_name="$1"
+    local desired_hostname="$1"
     local desired_ip="$2"
-    if ! grep -q "$desired_name" /etc/hosts; then
-        echo "$desired_ip $desired_name" >> /etc/hosts
-        log_changes "Added $desired_name with IP $desired_ip to /etc/hosts"
+    if ! grep -q "$desired_ip\s\+$desired_hostname" /etc/hosts; then
+        echo "$desired_ip $desired_hostname" >> /etc/hosts
+        log_message "Added $desired_hostname with IP $desired_ip to /etc/hosts"
     fi
 }
 
-trap '' TERM HUP INT
+trap 'exit 0' TERM HUP INT
 
-if [ -n "$SYS_INFO" ]; then
+if [ -n "$UPDATE_SYSTEM_INFO" ]; then
     update_system_info
 fi
 
-if [ -n "$NET_INFO" ]; then
+if [ -n "$UPDATE_NETWORK_INFO" ]; then
     update_network_info
 fi
 
-if [ -n "$HOST_ENTRY" ]; then
-    update_hosts_info
+if [ -n "$NEW_HOST_ENTRIES" ]; then
+    IFS=',' read -ra HOST_ENTRIES <<< "$NEW_HOST_ENTRIES"
+    for entry in "${HOST_ENTRIES[@]}"; do
+        IFS='=' read -ra host_info <<< "$entry"
+        update_host_entry "${host_info[1]}" "${host_info[0]}"
+    done
 fi
-
-exit 0
